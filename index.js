ebd156cab6f09d6a9e31d69cad0ecd458a939959
Iva Havranová <havranova.i@seznam.cz>
2023-04-23 12:17:14 +0200

    Emaily pomocí DOM elementů hotovo

--- index.js
+++ index.js
@@ -1,31 +1,25 @@
+import { Email } from './Email/index.js';
 const renderSection = (emails, element) => {
-  element.innerHTML = emails
-    .map((email) => {
-      let iconClass = 'opened';
-      if (email.unread) {
-        iconClass = 'closed';
-      };
+  element = emails.map((email) => {
+    let unread = 'opened';
+    if (email.unread) {
+      unread = 'closed';
+    }
 
-      return `
-        <div class="email">
-          <div class="email__head">
-            <button class="email__icon email__icon--${iconClass}"></button>
-            <div class="email__info">
-              <div class="email__sender">${email.sender.name}</div>
-              <div class="email__subject">${email.subject}</div>
-            </div>
-            <div class="email__time">${email.time}</div>
-          </div>
-          <div class="email__body"></div>
-        </div>
-      `;
-    })
-    .join('');
+    return (element.innerHTML += Email({
+      senderName: email.sender.name,
+      subject: email.subject,
+      time: email.time,
+      unread: email.unread,
+    }));
+  });
 };
 
 fetch(`https://apps.kodim.cz/daweb/trening-api/apis/emails?folder=unread`)
   .then((response) => response.json())
-  .then((data) => renderSection(data.emails, document.getElementById('unread')));
+  .then((data) =>
+    renderSection(data.emails, document.getElementById('unread')),
+  );
 
 fetch(`https://apps.kodim.cz/daweb/trening-api/apis/emails?folder=read`)
   .then((response) => response.json())
